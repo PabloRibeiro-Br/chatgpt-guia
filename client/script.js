ee129906b99793a60ca7d360c9986544b76ab630
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-03-07 01:16:21 -0300

    upupo

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://chatgpt-guia.vercel.app', {
+    const response = await fetch('https://chatgpt-guia.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
